82f56fc8c4653874cdb6c60bf91b9278f8624432
wbin <33045209+wbin327@users.noreply.github.com>
2019-05-21 17:13:15 +0800

    修改：修改util.py文件，修改为静态方法的写法

--- util.py
+++ util.py
@@ -2,23 +2,36 @@
 import os
 
 
-def mkdir(path):
-    """判断文件夹是否存在，不存在就创建"""
-    if not os.path.exists(path):
-        os.mkdir(path)
+class Util:
 
+    @staticmethod
+    def mkdir(path):
+        """判断文件夹是否存在，不存在就创建"""
+        if not os.path.exists(path):
+            os.mkdir(path)
 
-def get_path():
-    """获取当前文件所处的路径"""
-    path = os.getcwd()
-    return path
+    @staticmethod
+    def is_file(path: str, extension: list):
+        """
+        判断路径是否是文件，且该文件格式是否存在于集合中
+        :param path: 文件路径
+        :param extension: 文件后缀集合
+        :return: boolean
+        """
+        return os.path.isfile(path) and path.rsplit('.', 1)[1] in extension
 
+    @staticmethod
+    def get_path():
+        """获取当前文件所处的路径"""
+        path = os.getcwd()
+        return path
 
-def load_setting_from_obj(obj: object):
-    """
-    读取对象中所有自定义的属性，保存在一个字典中并返回
-    :param obj:对象
-    :return: attrs,保存了对象所有自定义属性的字典
-    """
-    attrs = {key: values for key, values in obj.__dict__.items() if not key.startswith('__')}
-    return attrs
+    @staticmethod
+    def load_setting_from_obj(obj: object):
+        """
+        读取对象中所有自定义的属性，保存在一个字典中并返回
+        :param obj:对象
+        :return: attrs,保存了对象所有自定义属性的字典
+        """
+        attrs = {key: values for key, values in obj.__dict__.items() if not key.startswith('__')}
+        return attrs
